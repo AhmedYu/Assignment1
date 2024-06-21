608474b257b2eb100cffb5fd9cdee7d113db7a1f
AhmedYu <abyan-adan@hotmail.com>
2024-06-21 15:51:04 -0500

    created centered top app bar with the color and the title

--- app/src/main/java/com/ahmed/assignment1/MainActivity.kt
+++ app/src/main/java/com/ahmed/assignment1/MainActivity.kt
@@ -4,44 +4,59 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ahmed.assignment1.ui.theme.Assignment1Theme
+import com.ahmed.assignment1.ui.theme.PurpleGrey40
+import com.ahmed.assignment1.ui.theme.PurpleGrey80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Assignment1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+       
         }
     }
 }
-
+//TODO:- Task: Create a composable function for the top bar with the application name
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainUI() {
+    Assignment1Theme {
+        Scaffold(topBar = {
+            CenterAlignedTopAppBar(title = {
+                Text(text = "Todo")
+            }, colors = TopAppBarDefaults.topAppBarColors(containerColor = PurpleGrey80, contentColorFor(
+                PurpleGrey80)),)
+        }, modifier = Modifier.fillMaxSize()){innerPadding->
+            //TODO:- TOP APP BAR WITH A TODOLIST NAME ON IT
+         Column(modifier = Modifier.padding(innerPadding)) {
+
+         }
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Assignment1Theme {
-        Greeting("Android")
+MainUI( )
     }
 }
