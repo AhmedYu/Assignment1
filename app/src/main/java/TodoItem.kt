6fdefbe6b3599fbe88b858289fdf94a3f856c207
AhmedYu <abyan-adan@hotmail.com>
2024-07-02 13:19:14 -0500

    added code comments on item class or todoitem class

--- app/src/main/java/TodoItem.kt
+++ app/src/main/java/TodoItem.kt
@@ -1,7 +1,12 @@
 import data.todoList
-
+//data class for the todo list
 data class TodoItem(var name: String, var isDone: Boolean)
 
+/**
+ * create a new todoItem
+ * @param entry : a string value
+ * returns nothing
+ */
 fun createNewTodoIem(entry: String) {
 
 	todoList.add(TodoItem(entry, false))
